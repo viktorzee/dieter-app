07a37d275fef8f758e435cf8ef2a0be17cb692fd
Victor Opara <opara.victor@gmail.com>
2023-02-02 23:23:51 +0100

    Added initial screens

---
+++ screens/ForgotPasswordSceen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ForgotPasswordSceen = () => {
+  return (
+    <View>
+      <Text>ForgotPasswordSceen</Text>
+    </View>
+  );
+};
+
+export default ForgotPasswordSceen;
+
+const styles = StyleSheet.create({});
